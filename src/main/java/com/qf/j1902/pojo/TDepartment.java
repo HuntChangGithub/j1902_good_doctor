fa2e92ceaa8748ba0a2a629290e7d4abfd73c34f
HuntChangGithub <525022859@qq.com>
2019-07-27 09:44:40 +0800

    add pojo tdepartment

---
+++ src/main/java/com/qf/j1902/pojo/TDepartment.java
@@ -0,0 +1,33 @@
+package com.qf.j1902.pojo;
+
+public class TDepartment {
+    private Integer depid;
+
+    private String depname;
+
+    private String deptimg;
+
+    public Integer getDepid() {
+        return depid;
+    }
+
+    public void setDepid(Integer depid) {
+        this.depid = depid;
+    }
+
+    public String getDepname() {
+        return depname;
+    }
+
+    public void setDepname(String depname) {
+        this.depname = depname == null ? null : depname.trim();
+    }
+
+    public String getDeptimg() {
+        return deptimg;
+    }
+
+    public void setDeptimg(String deptimg) {
+        this.deptimg = deptimg == null ? null : deptimg.trim();
+    }
+}
