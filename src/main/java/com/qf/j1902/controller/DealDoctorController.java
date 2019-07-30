edcd2edd982d52868db9477e125e5a813ba949f9
wanger <1213805522@qq.com>
2019-07-30 20:58:18 +0800

    wanger

--- src/main/java/com/qf/j1902/controller/DealDoctorController.java
+++ src/main/java/com/qf/j1902/controller/DealDoctorController.java
@@ -6,6 +6,7 @@ import com.qf.j1902.vo.DataVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -35,5 +36,25 @@ public class DealDoctorController {
         infoVo.setCount(allDocDealInfo.size());
         infoVo.setData(allDocDealInfo);
         return infoVo;
+    }
+    //审核通过
+    @ResponseBody
+    @RequestMapping("/main/dealdoctor/pass")
+    public String passto( Integer id){
+        boolean passto = docDealService.passto(id);
+        if (passto){
+            return "1";
+        }
+        return "0";
+    }
+    //冻结医生
+    @ResponseBody
+    @RequestMapping("/main/dealdoctor/seal")
+    public String seal(Integer id){
+        boolean sealDoc = docDealService.sealDoc(id);
+        if (sealDoc){
+            return "1";
+        }
+        return "0";
     }
 }
--- src/main/java/com/qf/j1902/service/impl/DocDealServiceImpl.java
+++ src/main/java/com/qf/j1902/service/impl/DocDealServiceImpl.java
@@ -23,4 +23,16 @@ public class DocDealServiceImpl implements DocDealService {
     public List<DocDealInfo> getAllDocDealInfo() {
         return docDealMapper.getAllDocDealInfo();
     }
+
+    @Override
+    public boolean passto(int id) {
+        int passto = docDealMapper.passto(id);
+        return passto>0;
+    }
+
+    @Override
+    public boolean sealDoc(int id) {
+        int i = docDealMapper.sealDoc(id);
+        return i>0;
+    }
 }
