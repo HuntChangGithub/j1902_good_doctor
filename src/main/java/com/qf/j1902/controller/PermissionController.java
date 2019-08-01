ce787ca074491b7fd3ec142718e91711a729faf8
wanger <1213805522@qq.com>
2019-08-01 22:25:40 +0800

    wanger

--- src/main/java/com/qf/j1902/controller/PermissionController.java
+++ src/main/java/com/qf/j1902/controller/PermissionController.java
@@ -6,14 +6,12 @@ import com.qf.j1902.vo.PermissionVo;
 import com.qf.j1902.vo.TransferVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @auther mengyuan
@@ -25,14 +23,25 @@ public class PermissionController {
     @Autowired
     private PermissionService permissionService;
 
+    //到权限管理的页面
     @RequestMapping("/main/permission")
-    public String permissionPage(Integer roleid){
+    public String permissionPage(){
+        return "permissionMa";
+    }
+
+    //根据角色id获取当前权限
+    @ResponseBody
+    @RequestMapping("/main/permission/data")
+    public List permissionData(@RequestParam("roleid") Integer roleid){
+        System.out.println("roleid"+roleid);
         List<PermissionVo> allPerByRoleId = permissionService.getAllPerByRoleId(roleid);
+        System.out.println("per"+allPerByRoleId);
         List<Integer> perlist = new ArrayList<>();
         for (PermissionVo per: allPerByRoleId) {
             perlist.add(per.getPerid());
+            System.out.println("aaaa"+perlist);
         }
-        return "permissionMa";
+        return perlist;
     }
 
     /**
@@ -45,13 +54,16 @@ public class PermissionController {
     @ResponseBody
     @RequestMapping("/main/permission/transfer")
     public String setRolePermission(String transferJson,Integer roleid ,String dofor){
+
         //将json转为对象
         List<TransferVo> transferVos = JSON.parseArray(transferJson, TransferVo.class);
+
         //将value取出，存入list
         List<Integer> pers = new ArrayList<>();
         for (TransferVo transVo:transferVos) {
             pers.add(transVo.getValue());
         }
+
         //做出增加或删除动作
         if ("l".equals(dofor)){//L说明从左边到右边，即为添加
             if (permissionService.setPer(pers, roleid)){//添加成功返回1
